ca7e746d748dbc66efb2f2438f92bf9b687376d8
Caio Capua <caiocapua@hotmail.com>
2021-06-04 16:29:00 -0300

    Componentes e estilizacao e iniciando miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return (
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amlount: 400,
+          type: 'Deposit',
+          category: 'Food',
+          create_at: new Date()
+        }
+      )
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
