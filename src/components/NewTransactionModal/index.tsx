29a00fe48847598b39a53c5f6058ea028bd8d4dd
Caio Capua <caiocapua@hotmail.com>
2021-06-09 17:47:02 -0300

    consumindo api miragejs, utizando o useContext, criando uma  transacao, listando a transacacao e calculo do resumo das transacoes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -12,21 +12,28 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const { createTransaction } = useTransactions()
+
     const [title, setTitle] = useState('');
     const [value, setValue] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {
-            title, 
-            value,
+        await createTransaction({
+            title,
+            amount: value,
             category,
             type
-        }
-        api.post('/transactions', data);
+        });
+
+        setTitle('');
+        setValue(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
